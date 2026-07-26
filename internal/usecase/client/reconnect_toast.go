01c510d7a6889a72494725e08138c6d13fa76cf8
brice <b@bnema.dev>
2026-07-26 07:20:45 +0200

    fix(client): reconcile remote probing toast

--- internal/usecase/client/reconnect_toast.go
+++ internal/usecase/client/reconnect_toast.go
@@ -47,18 +47,6 @@ func drawClientToast(out io.Writer, size domain.Size, message string) (domain.Re
 	return bounds, writeReconnectToast(out, bounds, reconnectToastLinesFor(bounds, message))
 }
 
-func clearReconnectToast(out io.Writer, bounds domain.Rect) error {
-	if bounds.Width <= 0 || bounds.Height <= 0 {
-		return nil
-	}
-	blank := strings.Repeat(" ", bounds.Width)
-	lines := make([]string, bounds.Height)
-	for i := range lines {
-		lines[i] = blank
-	}
-	return writeReconnectToast(out, bounds, lines)
-}
-
 func reconnectToastBounds(size domain.Size) domain.Rect {
 	return reconnectToastBoundsFor(size, reconnectToastMessage)
 }
